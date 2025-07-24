320f6357ebd9270de08c87cceebd338bfd46d79c
ihsanbisbox <ihsanbisbox@gmail.com>
2025-07-24 10:59:03 +0800

    ihsan baru mengubah

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        
+        // TAMBAHKAN INI - Panggil UserSeeder untuk membuat admin
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
